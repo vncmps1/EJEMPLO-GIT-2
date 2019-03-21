3c9637710b2a4e214ec2da26400fd807c84e6527
Iván Campos Iranzo <ivan.campos.iranzo@cieep.com>
2019-03-21 10:33:59 +0100

    Segunda Version

--- EJEMPLO GIT 2/Form1.cs
+++ EJEMPLO GIT 2/Form1.cs
@@ -16,5 +16,10 @@ namespace EJEMPLO_GIT_2
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Me quiero morir");
+        }
     }
 }
